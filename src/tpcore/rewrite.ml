c0030e4fb40a19e5f416f0316174742d18c4f5b6
mw <devnull@localhost>
2004-09-11 20:02:10 +0100

    Minor clean up.
    Add Rewrite.control, in preperation for major clean up.
    Remove simple rewriting option.

--- src/tpcore/rewrite.ml
+++ src/tpcore/rewrite.ml
@@ -10,7 +10,7 @@ type control =
       dir: direction
     }
 
-let mk_control i d=
+let control i d=
   { depth=i; dir=d }
 
 let limit_reached d = 
@@ -51,17 +51,6 @@ let find_match scp tyenv varp term1 term2 env=
   in 
   Gtypes.unify scp (Typing.typeof scp s) (Typing.typeof scp t)
 
-(*
-   let match_rewrite scp tyenv varp lhs rhs trm = 
-   let tenv = Term.empty_subst ()
-   in 
-   try
-   (ignore(find_match_aux scp tyenv varp lhs trm tenv); 
-   Term.subst_env tenv rhs)
-   with x -> 
-   (Term.addtermError "match_rewrite: failed" [lhs; trm] x)
- *)      
-
 let match_rewrite scp tyenv varp lhs rhs trm = 
   let env = Term.empty_subst ()
   in 
@@ -90,35 +79,13 @@ let rec make_rewrites_aux xs net=
     [] -> net
   | ((vs, key, rep)::rst) -> make_rewrites_aux rst
 	(Net.add (is_free_binder vs) net key (vs, key, rep))
-(*
-	(Net.enter (is_free_binder vs) (key, (vs, key, rep)) net)
-*)
 
 let make_rewrites xs = make_rewrites_aux (List.rev xs) (Net.empty())
-
-(* find list of possible rewrites in list *)
-
-(*
-   let find_rewrite_opts t rs =  rs
-
-   let rec find_first bs =
-   match bs with 
-   [] -> []
-   | ((q, b, c)::nxt) -> if (similar b t) then bs else  find_first nxt
-   and drop_last bs =
-   match bs with 
-   [] -> []
-   | ((q, b, c)::nxt) -> 
-   if (similar b t) then (q, b, c)::(drop_last nxt) else []
-   in drop_last (find_first rs)
- *)	
-
 
 type rewrite_rules = (Basic.binders list * Basic.term * Basic.term)
 type rewriteDB = 
     Net_rr of rewrite_rules Net.net 
-  | List_rr of rewrite_rules list
-
+  | List_rr of rewrite_rules list 
 
 let rec match_rr_list scp tyenv chng rs trm = 
   match rs with
@@ -140,7 +107,7 @@ let rec match_rewrite_list scp tyenv chng net trm =
   let rs=
     match net with
       (Net_rr n) -> Net.lookup n trm
-    | (List_rr r) -> r
+    | (List_rr r) -> r 
   in 
   let ntrm, ntyenv=match_rr_list scp tyenv cn rs trm
   in 
@@ -173,45 +140,12 @@ let rewrite_list_aux scp tyenv chng net trm =
   in 
   rewrite_aux tyenv trm
 
-(*  try
-   rewrite_aux trm
-   with x->
-   (Term.addtermError "rewrite failed for term" [trm] x)
- *)
 (* using term nets of rewrites *)
 
 let rewrite_list scp chng tyenv rs trm = 
   let nt=Net_rr(make_rewrites rs)
   in 
   rewrite_list_aux scp tyenv chng nt trm
-
-(*
-   let rec rewrite_list_aux  scp tyenv chng net trm = 
-   try
-   match trm with
-   Basic.Qnt(q, b) -> 
-   (let nb = rewrite_list_aux scp tyenv chng net b
-   in 
-   match_rewrite_list scp tyenv chng net (Qnt(q, nb)))
-   | Basic.Typed(tt, ty) -> 
-   rewrite_list_aux scp tyenv chng net tt
-   |	Basic.App(f, a)->
-   let nf = 
-   (rewrite_list_aux scp tyenv chng net f)
-   and na = 
-   (rewrite_list_aux scp tyenv chng net a)
-   in 
-   (match_rewrite_list scp tyenv chng net (Basic.App(nf, na)))
-   | _ -> (match_rewrite_list scp tyenv chng net trm)
-   with x -> 
-   (Term.addtermError "rewrite failed for term" [trm] x)
-
- *)
-
-(* (using lists of rewrite)
-   let rewrite_list scp chng rs trm = 
-   rewrite_list_aux scp (Gtypes.empty_subst()) chng (List_rr rs) trm
- *)
 
 let rewrite_net scp rn trm = 
   let chng = ref false
@@ -229,11 +163,6 @@ let rewrite_net_env scp tyenv rn trm =
   if !chng then (nt, nenv)
   else raise (termError "rewrite_net: no change" [trm])
 
-
-(*
-   let rewrite_bound chng scp qs t r trm = 
-   rewrite_bound_aux (chng, Term.empty_subst()) scp qs t r trm
- *)
 
 (* rewrite with equality eqtrm: "l=r" -> (l, r) 
    left-right if dir=true, right-left otherwise
@@ -259,79 +188,6 @@ let rewrite_eqs scp dir tyenv rrl trm =
   else raise (termError "Matching" [trm])
       
 
-(* simple rewriting: rewrite topdown once only  *)
-
-(*
-let simple_rewrite_list scp chng rs trm = 
-  let tyenv = Gtypes.empty_subst()
-  in 
-  let rec rewrite_aux tr = 
-    match tr with
-      Basic.Typed(tt, ty) -> 
-	rewrite_aux tt
-    | Basic.Qnt(q, b) ->
-	let ntrm = match_rr_list scp tyenv chng rs tr
-	in 
-	if (!chng) 
-	then 
-	  ntrm
-	else 
-	  (let nb= rewrite_aux b
-	  in 
-	  if(!chng)
-	  then Basic.Qnt(q, nb)
-	  else 
-	    raise (Term.termError "No match for rewrite" [tr]))
-    | _ -> match_rr_list scp tyenv chng rs tr
-  in 
-  try
-    rewrite_aux trm
-  with x -> (Term.addtermError "rewrite failed for term" [trm] x)
-*)
-
-let simple_rewrite_list scp chng tyenv rs trm = 
-  let rec rewrite_aux env tr = 
-    match tr with
-      Basic.Typed(tt, ty) -> 
-	rewrite_aux env tt
-    | Basic.Qnt(qnt, q, b) ->
-	let ntrm, nenv = match_rr_list scp env chng rs tr
-	in 
-	if (!chng) 
-	then (ntrm, nenv)
-	else 
-	  (let nb, benv= rewrite_aux nenv b
-	  in 
-	  if(!chng)
-	  then (Basic.Qnt(qnt, q, nb), benv)
-	  else 
-	    raise (Term.termError "No match for rewrite" [tr]))
-    | _ -> match_rr_list scp env chng rs tr 
-  in 
-  try
-    rewrite_aux tyenv trm
-  with x -> (Term.addtermError "rewrite failed for term" [trm] x)
-
-let simple_rewrite_eqs scp dir tyenv rrl trm =
-  let chng = ref false
-  in 
-  let r, ntyenv =
-    if dir
-    then simple_rewrite_list scp chng tyenv
-	(List.map (fun (qs, b)-> 
-	  let (a, b) =  Logicterm.dest_equal b in (qs, a, b)) rrl) 
-	trm
-    else simple_rewrite_list scp chng tyenv
-	(List.map 
-	   (fun (qs, b) -> 
-	     let (a, b)=Logicterm.dest_equal b in (qs, b, a)) rrl)
-	trm
-  in 
-  if !chng 
-  then r, ntyenv
-  else raise (termError "Matching" [trm])
-
-
 (* rewrite with equality term: "!x_1, ..., x_n. l=r" 
    -> [x_1, ..., x_n](l, r) 
    left-right if dir=true, right-left otherwise
@@ -340,26 +196,11 @@ let simple_rewrite_eqs scp dir tyenv rrl trm =
 let rewrite_univs scp ?(dir=true) ?(simple=false) rrl trm=
   let rs = List.map (strip_qnt Basic.All) rrl
   and tyenv = Gtypes.empty_subst()
-  in 
-  let nt, ntyenv= 
-    if simple 
-    then simple_rewrite_eqs scp dir tyenv rs trm
-    else rewrite_eqs scp dir tyenv rs trm
+  in let nt, _= rewrite_eqs scp dir tyenv rs trm
   in nt
-
-let rewrite_univ scp ?(dir=true) ?(simple=false) rr trm = 
-  rewrite_univs scp ~dir:dir ~simple:simple[rr] trm
 
 let rewrite_univs_env scp ?(dir=true) ?(simple=false) tyenv rrl trm=
   let rs = List.map (strip_qnt Basic.All) rrl
   in 
-  let nt, ntyenv=
-    if simple 
-    then simple_rewrite_eqs scp dir tyenv rs trm
-    else rewrite_eqs scp dir tyenv rs trm
-  in (nt, ntyenv)
-
-let rewrite_univ_env scp ?(dir=true) ?(simple=false) tyenv rr trm = 
-  rewrite_univs_env scp ~dir:dir ~simple:simple tyenv [rr] trm
-
+  rewrite_eqs scp dir tyenv rs trm
 
--- src/tpcore/rewrite.mli
+++ src/tpcore/rewrite.mli
@@ -5,6 +5,7 @@
 
 (* rewrite control, rules and databases *)
 type rewrite_rules = (Basic.binders list * Basic.term * Basic.term)
+
 type rewriteDB = 
     Net_rr of rewrite_rules Net.net 
   | List_rr of rewrite_rules list
@@ -17,7 +18,7 @@ type control =
       dir: direction
     }
 
-val mk_control : int option -> direction -> control
+val control : int option -> direction -> control
 
 (**
    [limit_reached d]
@@ -25,16 +26,20 @@ val mk_control : int option -> direction -> control
 *)
 val limit_reached: int option -> bool
 
-(* try to match a term with the LHS of a rewrite rule *)
-(* because lists of rewrites are used, the type contexts is important *)
+(** [find_match]
+   Try to match a term with the LHS of a rewrite rule
+   Because lists of rewrites are used, the type contexts is important 
+*)
     val find_match :
       Gtypes.scope -> Gtypes.substitution ->
       (Basic.term -> bool) ->
       Basic.term -> Basic.term -> Term.substitution 
 	-> Gtypes.substitution
 
-(* try to rewrite a term using a given rewrite rule
-   returns new term and updated type environment (substitution)
+(** [match_rewrite]
+
+   Try to rewrite a term using a given rewrite rule
+   Returns new term and updated type environment (substitution)
 *)
     val match_rewrite : 
 	Gtypes.scope -> Gtypes.substitution ->
@@ -42,40 +47,33 @@ val limit_reached: int option -> bool
 	    Basic.term -> Basic.term -> 
 	      (Basic.term* Gtypes.substitution)
 
-(* utility function to construct a predicate which tests 
-   for variables in unification *)
+(** [is_free_binder]
+   utility function to construct a predicate which tests 
+   for variables in unification 
+*)
     val is_free_binder : Basic.binders list -> Basic.term -> bool
 
-(* rewrite using a list of deconstructed rewrite rules *)
-(* return type environment built up during rewriting *)
+(** [rewrite_list]
+   rewrite using a list of deconstructed rewrite rules 
+   return type environment built up during rewriting 
+*)
     val rewrite_list :Gtypes.scope -> bool ref 
       -> Gtypes.substitution
 	  -> (Basic.binders list * Basic.term * Basic.term) list ->
 	      Basic.term -> (Basic.term * Gtypes.substitution)
 
-(* rewrite using a list of partialy deconstructed rewrite rules *)
-(* return type environment built up during rewriting *)
+(** [rewrite_eqs]
+   rewrite using a list of partialy deconstructed rewrite rules 
+   return type environment built up during rewriting 
+*)
     val rewrite_eqs :  Gtypes.scope -> bool 
       -> Gtypes.substitution
 	  -> (Basic.binders list * Basic.term)list 
 	    -> Basic.term -> (Basic.term * Gtypes.substitution )
 
-(* rewrite using a universally quantified rewrite rule *)
-(* dir: true for left to right, false for right to left *)
-(* simple: true for toplevel replacment only, 
-           false for full rewrite, recursing into term (default) *)
-(* simple rewriting: rewrite topmost term once only, 
-   recurse through quantifiers if necessary *)
-
-val rewrite_univ : Gtypes.scope ->  ?dir:bool -> ?simple:bool->
-    Basic.term -> Basic.term -> Basic.term
-
-val rewrite_univ_env : 
-    Gtypes.scope ->  ?dir:bool -> ?simple:bool
-	-> Gtypes.substitution 
-	  -> Basic.term -> Basic.term -> (Basic.term * Gtypes.substitution)
-
-(* rewrite using a list of universally quantified rewrite rules *)
+(** [rewrite_univs]
+   rewrite using a list of universally quantified rewrite rules 
+*)
     val rewrite_univs : 
 	Gtypes.scope -> ?dir:bool -> ?simple:bool ->
 	  Basic.term list -> Basic.term ->  Basic.term
@@ -86,7 +84,9 @@ val rewrite_univ_env :
 	    -> Basic.term list -> Basic.term 
 	      -> (Basic.term * Gtypes.substitution)
 
-(* rewrite using a database of rewrite rules *)
+(** [rewrite_net]
+ rewrite using a database of rewrite rules 
+*)
 val rewrite_net: Gtypes.scope -> rewriteDB -> Basic.term -> Basic.term
 
 val rewrite_net_env: 
