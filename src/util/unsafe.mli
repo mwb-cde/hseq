50b736aef352aa9bc584eef159d42afc2175f91a
mw <devnull@localhost>
2005-01-09 17:24:18 +0000

    *** empty log message ***

--- src/util/unsafe.mli
+++ src/util/unsafe.mli
@@ -3,5 +3,11 @@
    Utility functions which depend on unsafe/undocumented features.
 *)
 
+(**
+   [use_file ?silent name]
+
+   Use file [name] as a script. Equivalent to [#use name].
+   If [silent] is [true], no output, otherwise emits output like [#use].
+*)
 val use_file : ?silent:bool -> string -> unit
 
