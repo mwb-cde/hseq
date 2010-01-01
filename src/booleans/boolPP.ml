0b536458489b12a69c33db16315dddca2240e91c
mw <mwb.cde@googlemail.coml>
2010-01-01 12:54:00 +0100

    Change license to LGPL v3

--- src/booleans/boolPP.ml
+++ src/booleans/boolPP.ml
@@ -1,11 +1,26 @@
-(*-----
-  Name: boolPP.ml
-  Author: M Wahab <mwahab@users.sourceforge.net>
-  Copyright M Wahab 2006
-  ----*)
+(*----
+ Name: boolPP.ml
+ Copyright M Wahab 2006-2010
+ Author: M Wahab  <mwb.cde@googlemail.com>
+
+ This file is part of HSeq
+
+ HSeq is free software; you can redistribute it and/or modify it under
+ the terms of the Lesser GNU General Public License as published by
+ the Free Software Foundation; either version 3, or (at your option)
+ any later version.
+
+ HSeq is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
+ License for more details.
+
+ You should have received a copy of the Lesser GNU General Public
+ License along with HSeq.  If not see <http://www.gnu.org/licenses/>.
+----*)
 
 (**********
-	   * Printer-Parser for Boolean functions. 
+* Printer-Parser for Boolean functions. 
 **********)
 (**
    Printer for negation. Prints [ << base.not x >> ] 
--- src/util/tag.ml
+++ src/util/tag.ml
@@ -1,7 +1,22 @@
 (*----
  Name: tag.ml
- Author: M Wahab <mwahab@users.sourceforge.net>
- Copyright M Wahab 2005
+ Copyright M Wahab 2005-2010
+ Author: M Wahab  <mwb.cde@googlemail.com>
+
+ This file is part of HSeq
+
+ HSeq is free software; you can redistribute it and/or modify it under
+ the terms of the Lesser GNU General Public License as published by
+ the Free Software Foundation; either version 3, or (at your option)
+ any later version.
+
+ HSeq is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
+ License for more details.
+
+ You should have received a copy of the Lesser GNU General Public
+ License along with HSeq.  If not see <http://www.gnu.org/licenses/>.
 ----*)
 
 (* 
