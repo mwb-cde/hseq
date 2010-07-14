71bed50ba8f74d781aa5e9187b1e0f8f8d79ae8b
Matthew Wahab <mwb.cde@googlemail.com>
2010-07-14 17:48:12 +0100

    src/tpmain code clean-up.

--- src/tpmain/display.ml
+++ src/tpmain/display.ml
@@ -1,23 +1,23 @@
 (*----
- Name: display.ml
- Copyright M Wahab 2005-2010
- Author: M Wahab  <mwb.cde@googlemail.com>
+  Name: display.ml
+  Copyright M Wahab 2005-2010
+  Author: M Wahab  <mwb.cde@googlemail.com>
 
- This file is part of HSeq
+  This file is part of HSeq
 
- HSeq is free software; you can redistribute it and/or modify it under
- the terms of the Lesser GNU General Public License as published by
- the Free Software Foundation; either version 3, or (at your option)
- any later version.
+  HSeq is free software; you can redistribute it and/or modify it under
+  the terms of the Lesser GNU General Public License as published by
+  the Free Software Foundation; either version 3, or (at your option)
+  any later version.
 
- HSeq is distributed in the hope that it will be useful, but WITHOUT
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
- License for more details.
+  HSeq is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
+  License for more details.
 
- You should have received a copy of the Lesser GNU General Public
- License along with HSeq.  If not see <http://www.gnu.org/licenses/>.
-----*)
+  You should have received a copy of the Lesser GNU General Public
+  License along with HSeq.  If not see <http://www.gnu.org/licenses/>.
+  ----*)
 
 open Term
 open Format
@@ -25,13 +25,13 @@ open Report
 
 let cfun_string c =
   match c with 
-    "not" -> Format.printf "@[not@ @]"
-  | "and" -> Format.printf "@[and@ @]"
-  | "or" -> Format.printf "@[or@ @]"
-  | "implies" -> Format.printf "@[=>@ @]"
-  | "iff" -> Format.printf "@[<=>@ @]"
-  | "equals" -> Format.printf "@[=@ @]"
-  | x -> Format.printf "@[%s@ @]" x
+    | "not" -> Format.printf "@[not@ @]"
+    | "and" -> Format.printf "@[and@ @]"
+    | "or" -> Format.printf "@[or@ @]"
+    | "implies" -> Format.printf "@[=>@ @]"
+    | "iff" -> Format.printf "@[<=>@ @]"
+    | "equals" -> Format.printf "@[=@ @]"
+    | x -> Format.printf "@[%s@ @]" x
 
 let print_fnident x = Printer.print_ident x
 
@@ -63,35 +63,35 @@ let print_prf p = Goals.Proof.print (Global.PP.info()) p
 let print_prfstk p = Goals.ProofStack.print (Global.PP.info()) p
 
 let print_termdefn def = 
-    let n, ty, th = Logic.Defns.dest_termdef def
-    in 
-    Format.printf "@[";
-    Format.printf "@[";
-    print_fnident (Ident.mk_long Ident.null_thy (Ident.name_of n));
-    Format.printf ":@ ";
-    print_type ty;
-    Format.printf "@],@ ";
-    print_thm th;
-    Format.printf "@]"
+  let n, ty, th = Logic.Defns.dest_termdef def
+  in 
+  Format.printf "@[";
+  Format.printf "@[";
+  print_fnident (Ident.mk_long Ident.null_thy (Ident.name_of n));
+  Format.printf ":@ ";
+  print_type ty;
+  Format.printf "@],@ ";
+  print_thm th;
+  Format.printf "@]"
 
 let print_termdecln def = 
-    let n, ty = Logic.Defns.dest_termdecln def
-    in 
-    Format.printf "@[";
-    print_fnident (Ident.mk_long Ident.null_thy (Ident.name_of n));
-    Format.printf ":@ ";
-    print_type ty;
-    Format.printf "@]"
+  let n, ty = Logic.Defns.dest_termdecln def
+  in 
+  Format.printf "@[";
+  print_fnident (Ident.mk_long Ident.null_thy (Ident.name_of n));
+  Format.printf ":@ ";
+  print_type ty;
+  Format.printf "@]"
 
 
 let print_defn def =
   Logic.Defns.print_cdefn (Global.PP.info()) def
-  
+    
 let print_subst tenv f= 
   Format.printf "@[<2>";
-  (Hashtbl.iter 
-     (fun x y -> Format.printf "@[(%s =@ %s):@]@ " (f x) (f y))
-     tenv);
+  Hashtbl.iter 
+    (fun x y -> Format.printf "@[(%s =@ %s):@]@ " (f x) (f y))
+    tenv;
   Format.printf "@]"
     
 let print_error r = (r#print) (Global.PP.info())
--- src/tpmain/display.mli
+++ src/tpmain/display.mli
@@ -1,31 +1,31 @@
 (*----
- Name: display.mli
- Copyright M Wahab 2005-2010
- Author: M Wahab  <mwb.cde@googlemail.com>
+  Name: display.mli
+  Copyright M Wahab 2005-2010
+  Author: M Wahab  <mwb.cde@googlemail.com>
 
- This file is part of HSeq
+  This file is part of HSeq
 
- HSeq is free software; you can redistribute it and/or modify it under
- the terms of the Lesser GNU General Public License as published by
- the Free Software Foundation; either version 3, or (at your option)
- any later version.
+  HSeq is free software; you can redistribute it and/or modify it under
+  the terms of the Lesser GNU General Public License as published by
+  the Free Software Foundation; either version 3, or (at your option)
+  any later version.
 
- HSeq is distributed in the hope that it will be useful, but WITHOUT
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
- License for more details.
+  HSeq is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
+  License for more details.
 
- You should have received a copy of the Lesser GNU General Public
- License along with HSeq.  If not see <http://www.gnu.org/licenses/>.
-----*)
+  You should have received a copy of the Lesser GNU General Public
+  License along with HSeq.  If not see <http://www.gnu.org/licenses/>.
+  ----*)
 
 (** Top-level pretty printers *)
 
 open Basic
 
-val print_term : Basic.term -> unit
-val print_formula : Formula.t -> unit
-val print_type : Basic.gtype -> unit
+val print_term: Basic.term -> unit
+val print_formula: Formula.t -> unit
+val print_type: Basic.gtype -> unit
 val print_theory: Theory.thy -> unit
 
 val print_sqnt: Logic.Sequent.t -> unit
@@ -40,5 +40,5 @@ val print_prfstk: Goals.ProofStack.t -> unit
 
 val print_fnident: Ident.t -> unit
 
-val print_subst : ('a, 'a) Hashtbl.t -> ('a -> string) -> unit
+val print_subst: ('a, 'a)Hashtbl.t -> ('a -> string) -> unit
 val print_error: Report.error -> unit
