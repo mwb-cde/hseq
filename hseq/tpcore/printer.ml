dae1e48ed057321228d98920371a10dcedc79172
Matthew Wahab <mwb.cde@gmail.com>
2016-05-25 21:05:21 +0100

    Fix an extra comma when printing a pair.
    Also clean up some whitespace and update copyright date.

--- hseq/tpcore/printer.ml
+++ hseq/tpcore/printer.ml
@@ -1,7 +1,7 @@
 (*----
   Name: printer.ml
-  Copyright M Wahab 2005-2014
-  Author: M Wahab  <mwb.cde@gmail.com>
+  Copyright Matthew Wahab 2005-2016
+  Author: Matthew Wahab <mwb.cde@gmail.com>
 
   This file is part of HSeq
 
@@ -40,11 +40,11 @@ let right_assoc = Parserkit.Info.right_assoc
 (** Fixity of an identifier. *)
 type fixity = Parserkit.Info.fixity
 
-let nonfix = Parserkit.Info.nonfix 
+let nonfix = Parserkit.Info.nonfix
 let prefix = Parserkit.Info.prefix
 let suffix = Parserkit.Info.suffix
 
-let infix = Parserkit.Info.infix 
+let infix = Parserkit.Info.infix
 let infixl = Parserkit.Info.infix left_assoc
 let infixr = Parserkit.Info.infix right_assoc
 let infixn = Parserkit.Info.infix non_assoc
@@ -63,20 +63,20 @@ let is_suffix fx = Parserkit.Info.is_suffix fx
 
 (** String representation of fixity and associativity (for printing) *)
 
-let assoc_to_string a = 
+let assoc_to_string a =
   match a with
       Parserkit.Info.Leftassoc -> "left associative"
     | Parserkit.Info.Rightassoc -> "right associative"
     | Parserkit.Info.Nonassoc -> "non-associative"
 
-let fixity_to_string fx = 
+let fixity_to_string fx =
   match fx with
       Parserkit.Info.Nonfix -> "nonfix"
     | Parserkit.Info.Prefix -> "prefix"
     | Parserkit.Info.Suffix -> "suffix"
-    | Parserkit.Info.Infix(a) -> 
+    | Parserkit.Info.Infix(a) ->
       ("infix ("^(assoc_to_string a)^")")
-	
+
 (** Default precedence, fixity and assoc *)
 
 let default_term_prec = 10
@@ -91,16 +91,16 @@ let default_type_fixity = Parserkit.Info.nonfix
 let fun_app_prec = 90
 
 (** Precedence of Quantifiers *)
-let prec_qnt q = 
-  match q with 
+let prec_qnt q =
+  match q with
       Lambda -> 60
     | All -> 55
     | Ex -> 55
     | _ -> 55
 
 (** Associativity of Quantifiers *)
-let assoc_qnt q = 
-  match q with 
+let assoc_qnt q =
+  match q with
       Lambda -> non_assoc
     | All -> non_assoc
     | Ex -> non_assoc
@@ -117,14 +117,14 @@ let fixity_qnt q = nonfix
 type 'a printer = 'a -> unit
 
 (** The print record of an identifier. *)
-type record = 
+type record =
     {
       prec: int;            (* its precedence *)
       fixity: fixity;       (* its fixity *)
       repr: string option   (* its representation *)
-    } 
+    }
 
-let mk_record pr inf rep = 
+let mk_record pr inf rep =
   {prec = pr; fixity = inf; repr = rep}
 
 let empty_record() = mk_record (-1) nonfix None
@@ -134,13 +134,13 @@ let empty_record() = mk_record (-1) nonfix None
  *)
 
 (** The table of records and printers for a set of identifiers. *)
-type ('a, 'b) info = 
+type ('a, 'b) info =
     {
       records: (record) Ident.tree;
       printers: ('a -> (fixity * int) -> ('b printer)) Ident.tree
     }
 
-let mk_info sz = 
+let mk_info sz =
   {records = Ident.Tree.empty(); printers = Ident.Tree.empty()}
 
 let default_info_size = 53
@@ -148,53 +148,53 @@ let empty_info() = mk_info default_info_size
 
 (** Add/access printer records *)
 
-let get_record info id = 
+let get_record info id =
   Ident.Tree.find info.records id
-let add_record info id rcrd = 
+let add_record info id rcrd =
   { info with records = Ident.Tree.replace info.records id rcrd }
-let remove_record info id  = 
+let remove_record info id  =
   { info with records = Ident.Tree.remove info.records id }
 
 (** Construct printer records to be added/accessed from [info] *)
 
-let get_info info id = 
-  try 
-    let r = Ident.Tree.find (info.records) id in 
+let get_info info id =
+  try
+    let r = Ident.Tree.find (info.records) id in
     (r.prec, r.fixity, r.repr)
   with Not_found -> (default_term_prec, default_term_fixity, None)
 
-let add_info (inf:('a, 'b)info) id pr fx rp = 
+let add_info (inf:('a, 'b)info) id pr fx rp =
   let r = {prec = pr; fixity = fx; repr = rp}
-  in 
+  in
   add_record inf id r
 
-let remove_info info id = 
+let remove_info info id =
   { info with records = Ident.Tree.remove info.records id }
 
 (** User defined printers *)
 
-let get_printer info id = 
-  Ident.Tree.find info.printers id 
+let get_printer info id =
+  Ident.Tree.find info.printers id
 
-let add_printer info id prntr = 
+let add_printer info id prntr =
   { info with printers = Ident.Tree.add info.printers id prntr }
 
-let remove_printer info id = 
+let remove_printer info id =
   { info with printers = Ident.Tree.remove info.printers id }
 
 
 (*
- * Combined printer information tables} 
+ * Combined printer information tables}
  *)
 
 (** The combined printer information for terms and types. *)
-type ppinfo = 
+type ppinfo =
     {
       terms:  (ppinfo, Basic.term * (Basic.term)list) info;
       types:  (ppinfo, Ident.t * (Basic.gtype)list) info
     }
 
-let mk_ppinfo sz = 
+let mk_ppinfo sz =
   {
     terms = mk_info sz;
     types = mk_info sz;
@@ -203,51 +203,51 @@ let empty_ppinfo() = mk_ppinfo default_info_size
 
 (** Operations involving term identifiers *)
 
-type term_printer = 
+type term_printer =
   ppinfo -> (fixity * int) -> (Basic.term * Basic.term list) printer
 
 let get_term_info info x = get_info (info.terms) x
 let set_term_info info x = {info with terms = x}
-let add_term_info info id prec fixity repr = 
+let add_term_info info id prec fixity repr =
   set_term_info info (add_info (info.terms) id prec fixity repr)
-let add_term_record info id record = 
+let add_term_record info id record =
   set_term_info info (add_record (info.terms) id record)
-let remove_term_info info id = 
+let remove_term_info info id =
   set_term_info info (remove_info (info.terms) id)
 
-let get_term_printer info x = 
+let get_term_printer info x =
   get_printer (info.terms) x
-let add_term_printer info id prnt = 
+let add_term_printer info id prnt =
   set_term_info info (add_printer (info.terms) id prnt)
-let remove_term_printer info id = 
+let remove_term_printer info id =
   set_term_info info (remove_printer (info.terms) id)
 
 (** Operations involving type identifiers *)
-type gtype_printer = 
+type gtype_printer =
   ppinfo -> (fixity * int) -> (Ident.t * (Basic.gtype list)) printer
 let get_type_info info x = get_info (info.types) x
 let set_type_info info x = {info with types = x}
-let add_type_info info id prec fixity repr = 
+let add_type_info info id prec fixity repr =
   set_type_info info (add_info (info.types) id prec fixity repr)
-let add_type_record info id record = 
+let add_type_record info id record =
   set_type_info info (add_record (info.types) id record)
-let remove_type_info info id = 
+let remove_type_info info id =
   set_type_info info (remove_info (info.types) id)
 
 let get_type_printer info x = get_printer (info.types) x
-let add_type_printer info id prnt = 
+let add_type_printer info id prnt =
   set_type_info info (add_printer (info.types) id prnt)
-let remove_type_printer info id = 
+let remove_type_printer info id =
   set_type_info info (remove_printer (info.types) id)
 
 (*
  * Pretty-printing utility functions
  *)
 
-let string_identifier id pp_rec = 
+let string_identifier id pp_rec =
   match pp_rec.repr
-  with 
-    | None -> (Ident.string_of id) 
+  with
+    | None -> (Ident.string_of id)
     | Some(x) -> x
 
 (** Print a string *)
@@ -257,66 +257,67 @@ let print_string str = Format.printf "%s" str
 let print_space _ =  Format.printf "@ "
 
 (** Print a precedence marking bracket. *)
-let print_bracket nprec prec br = 
+let print_bracket nprec prec br =
   if prec < nprec then Format.printf "%s" br else ()
 
 (** Print a precedence and fixity aware bracket *)
-let print_assoc_bracket cr nr br = 
+let print_assoc_bracket cr nr br =
   let (cassoc, cprec) = cr
   and (nassoc, nprec) = nr
-  in 
+  in
   if nprec < cprec
-  then Format.printf "%s" br 
-  else 
+  then Format.printf "%s" br
+  else
     if nprec = cprec
-    then 
+    then
       (if (is_non_assoc cassoc || is_non_assoc nassoc)
        then ()
-       else 
+       else
 	  (if not (cassoc = nassoc)
 	   then Format.printf "%s" br
 	   else ()))
     else ()
 
 (** List printer *)
-let print_list (term_printer, sep) x = 
-  let rec print_aux ts = 
-    match ts with 
+let print_list (term_printer, sep) x =
+  let rec print_aux ts =
+    match ts with
       | [] -> ()
-      | [p] -> term_printer p
-      | p::ps -> 
-	(term_printer p;
-	 sep (); 
-	 Format.printf "@,";
-	 print_aux ps)
-  in 
+      | p::[] -> term_printer p
+      | p::ps ->
+	 begin
+           term_printer p;
+	   sep ();
+	   print_aux ps
+         end
+  in
   print_aux x
 
 (** Print a list with a given string as seperator *)
-let print_sep_list (term_printer, sep) x = 
+let print_sep_list (term_printer, sep) x =
   print_list (term_printer, (fun _ -> Format.printf "%s@ " sep)) x
 
 (** Printer a simple identifier *)
-let print_ident x = 
+let print_ident x =
   Format.printf "%s" (Ident.string_of x)
 
 (** Print an identifier, using stored representation if available *)
-let print_identifier info x = 
-  let pprec = info x in 
-  let str = 
+let print_identifier info x =
+  let pprec = info x in
+  let str =
     (match pprec.repr with
-      | None -> 
+      | None ->
 	if (!Settings.long_identifier)
 	then Ident.string_of x
 	else Ident.name_of x
       | Some(s) -> s)
-  in 
+  in
   Format.printf "%s" str
 
 (** Print as infix *)
-let print_infix (opr, tpr) prec (op, trms) = 
+let print_infix (opr, tpr) prec (op, trms) =
   match trms with
-    | l::rargs -> 
+    | l::rargs ->
       Format.printf "@[<2>";
       tpr prec [l];
       Format.printf "@ ";
@@ -324,13 +325,13 @@ let print_infix (opr, tpr) prec (op, trms) =
       Format.printf "@ ";
       tpr prec rargs;
       Format.printf "@]"
-    | [] -> 
+    | [] ->
       Format.printf "@[<2>";
       opr prec op;
       Format.printf "@]"
 
 (** Print as suffix *)
-let print_suffix (opr, tpr) prec (op, trms) = 
+let print_suffix (opr, tpr) prec (op, trms) =
   Format.printf "@[<2>";
   tpr prec trms;
   Format.printf "@ ";
@@ -338,30 +339,29 @@ let print_suffix (opr, tpr) prec (op, trms) =
   Format.printf "@]"
 
 (** Print as prefix *)
-let print_prefix (opr, tpr) prec (op, trms) = 
+let print_prefix (opr, tpr) prec (op, trms) =
   Format.printf "@[<2>";
   opr prec op;
   Format.printf "@ ";
   tpr prec trms;
   Format.printf "@]"
 
-(** 
+(**
     Print an operator, looking up information to determine whether it
     is infix, suffix or prefix.
 *)
-let print_operator (opr, tpr, ppinfo) prec (op, trms) = 
-  let pprec = ppinfo op in 
-  let printer = 
+let print_operator (opr, tpr, ppinfo) prec (op, trms) =
+  let pprec = ppinfo op in
+  let printer =
     if (is_infix pprec.fixity)
     then print_infix (opr, tpr)
-    else 
+    else
       if (is_suffix pprec.fixity)
       then print_suffix (opr, tpr)
       else print_prefix (opr, tpr)
-  in 
+  in
   Format.printf "@[<2>";
   print_bracket prec (pprec.prec) "(";
   printer (pprec.prec) (op, trms);
-  print_bracket prec (pprec.prec) ")"; 
+  print_bracket prec (pprec.prec) ")";
   Format.printf "@]"
-
--- thys/PairScript.ml
+++ thys/PairScript.ml
@@ -1,25 +1,25 @@
 (*----
  Name: PairScript.ml
- Copyright M Wahab 2005-2013
- Author: M Wahab  <mwb.cde@gmail.com>
+ Copyright Matthew Wahab 2005-2016
+ Author: Matthew Wahab <mwb.cde@gmail.com>
 
- This file is part of HSeq
+  This file is part of HSeq
 
- HSeq is free software; you can redistribute it and/or modify it under
- the terms of the Lesser GNU General Public License as published by
- the Free Software Foundation; either version 3, or (at your option)
- any later version.
+  HSeq is free software; you can redistribute it and/or modify it under
+  the terms of the Lesser GNU General Public License as published by
+  the Free Software Foundation; either version 3, or (at your option)
+  any later version.
 
- HSeq is distributed in the hope that it will be useful, but WITHOUT
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
- License for more details.
+  HSeq is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
+  License for more details.
 
- You should have received a copy of the Lesser GNU General Public
- License along with HSeq.  If not see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the Lesser GNU General Public
+  License along with HSeq.  If not see <http://www.gnu.org/licenses/>.
 ----*)
 
-(** 
+(**
    Product of types.
 *)
 
@@ -36,13 +36,13 @@ let pair_fixity = PairLib.pair_fixity
 
 (** {5 Definition and basic properties of pairs} *)
 
-let mk_pair_def = 
+let mk_pair_def =
   define <:def< mk_pair x y = (%a b: (a=x) and (b=y)) >>;;
 
-let is_pair_def = 
+let is_pair_def =
   define <:def< is_pair p = (?x y: p = (mk_pair x y)) >>;;
 
-let pair_exists = 
+let pair_exists =
   prove_thm "pair_exists" << ? p: is_pair p >>
   [
     unfold "is_pair";
@@ -50,38 +50,38 @@ let pair_exists =
     eq_tac
   ];;
 
-let mk_pair_is_pair = 
+let mk_pair_is_pair =
   prove_thm "mk_pair_is_pair" << !x y : is_pair (mk_pair x y) >>
   [
     flatten_tac; unfold "is_pair" ; inst_tac [ << _x >>; << _y >> ]; eq_tac
   ];;
 
-let pair_tydef = 
+let pair_tydef =
   typedef ~pp:(pair_prec, infixr, Some("*"))
     ~thm:pair_exists
     ~rep:"dest_PAIR" ~abs:"make_PAIR"
     <:def<: ('a, 'b)PAIR = ('a -> 'b -> bool): is_pair >>;;
 
-let pair_def= define 
-    ~pp:(pair_prec, pair_fixity, Some(",")) 
+let pair_def= define
+    ~pp:(pair_prec, pair_fixity, Some(","))
     <:def< pair x y = make_PAIR (mk_pair x y) >> ;;
 
 let fst_def = define
     <:def< fst p = (epsilon(% x: ?y: p = (pair x y))) >>;;
 
-let snd_def = define 
+let snd_def = define
     <:def< snd p = epsilon(% y: ?x: p = (pair x y)) >>;;
 
-let mk_pair_eq = 
+let mk_pair_eq =
   prove_thm "mk_pair_eq"
     <<
-  ! a b x y: 
-    ((mk_pair a b) = (mk_pair x y)) 
+  ! a b x y:
+    ((mk_pair a b) = (mk_pair x y))
     =
   ((a = x) and (b = y))
     >>
   [flatten_tac ++ equals_tac ++ iffE
-      -- 
+      --
       [ (* cut_back_tac mk_pair_eq1 ++ basic; *)
 	(match_asm << X = Y >>
 	 (fun l -> once_rewrite_tac ?dir:None [thm "function_eq"] ~f:l))
@@ -89,7 +89,7 @@ let mk_pair_eq =
 	  ++ (match_asm << L = R >>
 	      (fun l -> once_rewrite_tac ?dir:None [thm "function_eq"] ~f:l))
 	  ++ inst_tac [ << _b >> ]
-	  ++ (match_asm << A = B >> 
+	  ++ (match_asm << A = B >>
 	      (fun l -> once_rewrite_tac [thm "eq_sym"] ~f:l))
 	  ++ unfold "mk_pair" ++ beta_tac
 	  ++ replace_tac
@@ -109,8 +109,8 @@ let rep_abs_pair=
      ++ basic
  ];;
 
-let pair_thm = 
-  prove_thm "pair_thm" 
+let pair_thm =
+  prove_thm "pair_thm"
     << ! x y: (dest_PAIR (pair x y)) = (mk_pair x y) >>
   [
    flatten_tac ++ unfold "pair"
@@ -120,21 +120,21 @@ let pair_thm =
  ];;
 
 
-let inj_on_make_PAIR = 
+let inj_on_make_PAIR =
   prove_thm "inj_on_make_PAIR"
     << inj_on make_PAIR is_pair >>
   [
    cut_thm "inj_on_inverse_intro"
      ++ inst_tac [ << make_PAIR >>; << is_pair >>; << dest_PAIR >> ]
-     ++ cut_thm "make_PAIR_inverse" 
-     ++ split_tac 
+     ++ cut_thm "make_PAIR_inverse"
+     ++ split_tac
      -- [ basic; basic ]
  ];;
 
 
 (** {5 Properties} *)
 
-let basic_pair_eq = 
+let basic_pair_eq =
   prove_thm "basic_pair_eq"
     << ! a b x y: ((pair a b) = (pair x y)) = ((a = x) and (b = y)) >>
   [
@@ -163,19 +163,19 @@ let basic_pair_eq =
 ;;
 
 
-let fst_thm = 
-  prove_thm "fst_thm" ~simp:true 
+let fst_thm =
+  prove_thm "fst_thm" ~simp:true
     << ! x y: (fst (pair x y)) = x >>
   [
    flatten_tac ++ unfold "fst"
      ++ cut_thm "epsilon_ax"
      ++ inst_tac [ << %a: ?b: (pair _x _y) = (pair a b) >> ]
-     ++ beta_tac 
-     ++ split_tac 
+     ++ beta_tac
+     ++ split_tac
      --
      [
-      inst_tac [ << _x >> ; << _y >> ] ++ eq_tac; 
-      flatten_tac 
+      inst_tac [ << _x >> ; << _y >> ] ++ eq_tac;
+      flatten_tac
 	++ rewrite_tac [basic_pair_eq]
 	++ flatten_tac
 	++ (replace_tac ~dir:rightleft)
@@ -183,19 +183,19 @@ let fst_thm =
     ]
  ];;
 
-let snd_thm = 
-  prove_thm "snd_thm" ~simp:true 
+let snd_thm =
+  prove_thm "snd_thm" ~simp:true
     << ! x y: (snd (pair x y)) = y >>
   [
    flatten_tac ++ unfold "snd"
      ++ cut_thm "epsilon_ax"
      ++ inst_tac [ << %b: ?a: (pair _x _y) = (pair a b) >> ]
-     ++ beta_tac 
-     ++ split_tac 
+     ++ beta_tac
+     ++ split_tac
      --
      [
-      inst_tac [ << _y >>; << _x >> ] ++ eq_tac; 
-      flatten_tac 
+      inst_tac [ << _y >>; << _x >> ] ++ eq_tac;
+      flatten_tac
 	++ rewrite_tac [basic_pair_eq]
 	++ flatten_tac
 	++ (replace_tac ~dir:rightleft)
@@ -203,55 +203,55 @@ let snd_thm =
     ]
  ];;
 
-let pair_inj = 
+let pair_inj =
   prove_thm "pair_inj"
     << ! p: ?x y: p = (pair x y) >>
   [
    flatten_tac ++ unfold "pair"
      ++ cut ~inst:[ << _p >> ](thm "dest_PAIR_mem")
-     ++ cut ~inst:[ << _p >> ] (thm "dest_PAIR_inverse") 
+     ++ cut ~inst:[ << _p >> ] (thm "dest_PAIR_inverse")
      ++ unfold "is_pair"
      ++ flatten_tac
      ++ inst_tac [ << _x >> ; << _y >> ]
-     ++ (match_asm << (dest_PAIR x) = Y >> 
+     ++ (match_asm << (dest_PAIR x) = Y >>
 	 (fun l -> replace_tac ~dir:rightleft ~asms:[l] ?f:None))
-     ++ (match_asm << (make_PAIR (dest_PAIR x)) = Y >> 
+     ++ (match_asm << (make_PAIR (dest_PAIR x)) = Y >>
 	 (fun l -> replace_tac ?dir:None ~asms:[l] ?f:None))
      ++ eq_tac
  ];;
 
-let pair_cases = 
+let pair_cases =
   save_thm "PAIR_cases" pair_inj
 
-let pair_induct = 
+let pair_induct =
   theorem "PAIR_induct"
   << !P: (! x y: (P (x, y))) => (!x: P x) >>
   [
     flatten_tac
    ++ cut pair_cases
    ++ instA [ << _x >> ]
-   ++ specA 
+   ++ specA
    ++ replace_tac ++ unify_tac
   ]
 
-let surjective_pairing = 
-  prove_thm "surjective_pairing" ~simp:true 
+let surjective_pairing =
+  prove_thm "surjective_pairing" ~simp:true
     << !p: (pair (fst p) (snd p)) = p >>
   [
    flatten_tac
-     ++ cut ~inst:[ << _p >> ] pair_inj 
+     ++ cut ~inst:[ << _p >> ] pair_inj
      ++ flatten_tac
      ++ replace_tac
      ++ rewrite_tac [basic_pair_eq; fst_thm; snd_thm]
      ++ (split_tac ++ eq_tac)
  ];;
 
-let pair_eq = 
-  prove_thm "pair_eq" 
+let pair_eq =
+  prove_thm "pair_eq"
     << ! p q : (p = q) = (((fst p) = (fst q)) and ((snd p) = (snd q))) >>
   [
-   flatten_tac 
-     ++ cut ~inst:[ << fst _p >>; << snd _p >> ; 
+   flatten_tac
+     ++ cut ~inst:[ << fst _p >>; << snd _p >> ;
 	       << fst _q >>; << snd _q >> ] basic_pair_eq
      ++ rewrite_tac [surjective_pairing]
      ++ basic
@@ -259,5 +259,3 @@ let pair_eq =
 
 
 let _ = end_theory();;
-
-
