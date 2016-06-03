7afeb2eec4c529c8789b1ea537eb1f902982d89e
Matthew Wahab <mwb.cde@gmail.com>
2016-06-03 20:01:52 +0100

    [thys] Make independent of the staging directory.

--- thys/0MainScript.ml
+++ thys/0MainScript.ml
@@ -1,26 +1,25 @@
 (*----
  Name: 0MainScript.ml
- Copyright M Wahab 2005-2013
- Author: M Wahab  <mwb.cde@gmail.com>
+ Copyright Matthew Wahab 2005-2016
+ Author: Matthew Wahab <mwb.cde@gmail.com>
 
  This file is part of HSeq
 
- HSeq is free software; you can redistribute it and/or modify it under
- the terms of the Lesser GNU General Public License as published by
- the Free Software Foundation; either version 3, or (at your option)
- any later version.
+  HSeq is free software; you can redistribute it and/or modify it under the
+  terms of the Lesser GNU General Public License as published by the Free
+  Software Foundation; either version 3, or (at your option) any later version.
 
- HSeq is distributed in the hope that it will be useful, but WITHOUT
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
- License for more details.
+  HSeq is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public License for
+  more details.
 
- You should have received a copy of the Lesser GNU General Public
- License along with HSeq.  If not see <http://www.gnu.org/licenses/>.
-----*)
+  You should have received a copy of the Lesser GNU General Public License
+  along with HSeq.  If not see <http://www.gnu.org/licenses/>.
+  ----*)
 
 (**
-   Create theory "Main" 
+   Create theory "Main"
    Theory Main is the top-level theory (the "base theory") for the
    standard theory library.  It is imported into all theories (except
    those in the standard library).
@@ -29,27 +28,27 @@
    the base theory name is zero'd out while the standard library is built.
 
    This script is called 0MainScript to avoid it being called from the
-   theories that Main depends on.    
+   theories that Main depends on.
 *)
 
 (* Clear the base theory name. *)
-let mainScript_base_name = 
-  let str = 
-    try Context.base_name(Global.context()) 
+let mainScript_base_name =
+  let str =
+    try Context.base_name(Global.context())
     with _ -> "Main"
-  in 
-  Global.set_context(Context.clear_base_name (Global.context())); 
+  in
+  Global.set_context(Context.clear_base_name (Global.context()));
   str;;
 
 (** Build theory Main and the theories it depends on *)
-let _ = 
-begin_theory "Main" 
- ["Set"; "Sum"; "Pair"; "Fun"; "Relation"; "Bool"; "base"];; 
+let _ =
+begin_theory "Main"
+ ["Set"; "Sum"; "Pair"; "Fun"; "Relation"; "Bool"; "base"];;
 
 let _ = end_theory();;
 
 (* Reset the base theory name *)
-let _ = 
-  Global.set_context(Context.set_base_name 
-                       (Global.context()) 
+let _ =
+  Global.set_context(Context.set_base_name
+                       (Global.context())
                        mainScript_base_name);;
--- thys/buildall.ml
+++ thys/buildall.ml
@@ -1,28 +1,27 @@
 (*----
  Name: buildall.ml
- Copyright M Wahab 2005-2013
- Author: M Wahab  <mwb.cde@gmail.com>
+ Copyright Matthew Wahab 2005-2016
+ Author: Matthew Wahab <mwb.cde@gmail.com>
 
  This file is part of HSeq
 
- HSeq is free software; you can redistribute it and/or modify it under
- the terms of the Lesser GNU General Public License as published by
- the Free Software Foundation; either version 3, or (at your option)
- any later version.
+  HSeq is free software; you can redistribute it and/or modify it under the
+  terms of the Lesser GNU General Public License as published by the Free
+  Software Foundation; either version 3, or (at your option) any later version.
 
- HSeq is distributed in the hope that it will be useful, but WITHOUT
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
- License for more details.
+  HSeq is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public License for
+  more details.
 
- You should have received a copy of the Lesser GNU General Public
- License along with HSeq.  If not see <http://www.gnu.org/licenses/>.
-----*)
+  You should have received a copy of the Lesser GNU General Public License
+  along with HSeq.  If not see <http://www.gnu.org/licenses/>.
+  ----*)
 
 (**
    Build all theories.
 
-   Run from a shell with 
+   Run from a shell with
    `hseq -I INCLUDEDIR buildall.ml`
    where INCLUDEDIR is the path to the hseq include directory.
    e.g. hseq -I ../include buildall
@@ -36,12 +35,10 @@ open HSeq
 open HSeqUser
 open Userlib
 
-
 (** Add the source include directories *)
-let _ = 
- Settings.include_dirs :=
-    "../obj/lib":: (!Settings.include_dirs);;
-    
+let _ =
+  Settings.include_dirs :=
+    "../hseq"::"../hsequser"::(!Settings.include_dirs);;
 
 (** Initialise hseq *)
 #use "hseqstart.ml";;
@@ -49,20 +46,12 @@ let _ =
 (**
    This is part of the standard library so clear the theory base name.
 *)
-let buildall_base_name = 
-  let str = 
-    try Context.base_name(Global.context()) 
+let buildall_base_name =
+  let str =
+    try Context.base_name(Global.context())
     with _ -> "Main"
-  in 
-  Global.set_context(Context.clear_base_name (Global.context())); 
+  in
+  Global.set_context(Context.clear_base_name (Global.context()));
   str;;
 
 #use "0MainScript.ml";;
-
-(***
-
-(* Reset the base theory name *)
-let _ = Global.Thys.set_base_name buildall_base_name;;
-
-
-***)
