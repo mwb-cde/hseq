7a4df5eefcf46ec3b0ec0ef23e8160482d774a2d
Matthew Wahab <mwb.cde@gmail.com>
2016-10-16 16:00:38 +0100

    [hseqtop] Use Dynlink for library loading.
    Also clean up some code.

--- tools/hseqtop/unsafe.ml
+++ tools/hseqtop/unsafe.ml
@@ -1,64 +1,54 @@
 (*----
- Name: unsafe.ml
- Copyright M Wahab 2005-2010
- Author: M Wahab  <mwb.cde@googlemail.com>
+  Name: unsafe.ml
+  Copyright Matthew Wahab 2005-2016
+  Author: Matthew Wahab <mwb.cde@googlemail.com>
 
- This file is part of HSeq
+  This file is part of HSeq
 
- HSeq is free software; you can redistribute it and/or modify it under
- the terms of the Lesser GNU General Public License as published by
- the Free Software Foundation; either version 3, or (at your option)
- any later version.
+  HSeq is free software; you can redistribute it and/or modify it under
+  the terms of the Lesser GNU General Public License as published by
+  the Free Software Foundation; either version 3, or (at your option)
+  any later version.
 
- HSeq is distributed in the hope that it will be useful, but WITHOUT
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
- License for more details.
+  HSeq is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
+  License for more details.
 
- You should have received a copy of the Lesser GNU General Public
- License along with HSeq.  If not see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the Lesser GNU General Public
+  License along with HSeq.  If not see <http://www.gnu.org/licenses/>.
 ----*)
 
 (** Utility functions which depend on unsafe/undocumented features. *)
 
-let use_string st =  
+let use_string st =
  try
-   ignore (Toploop.execute_phrase true Format.std_formatter 
+   ignore (Toploop.execute_phrase true Format.std_formatter
              ((!Toploop.parse_toplevel_phrase) (Lexing.from_string st)))
  with _ -> failwith ("use_string "^st);;
 
 
-let use_file ?(silent=false) f = 
+let use_file ?(silent=false) f =
   if silent
   then ignore(Toploop.use_silently Format.std_formatter f)
   else ignore(Toploop.use_file Format.std_formatter f)
 
-(*
-let load_file f = Topdirs.dir_load Format.std_formatter f
-*)
-let load_file f = ignore(Topdirs.load_file Format.std_formatter f)
+let load_file = Dynlink.loadfile
 
 let add_directory s = Topdirs.dir_directory s
 
-(** 
-   Functions to add an init function [f] to be called when
-   the OCaml toplevel starts. 
-*)
-
-
-(** [add_init_306 f]: Setup function [f] to be called when OCaml
+(** [add_init f]: Setup function [f] to be called when OCaml
     starts.
 
     Use Toploop.parse_toplevel_phrase to call function [f] then
     restore Toploop.parse_toplevel_phrase to original (ocaml) value
     once init() has been called.
 *)
-let add_init f = 
+let add_init f =
   let ocaml_init = !Toploop.parse_toplevel_phrase
-  in 
+  in
   Toploop.parse_toplevel_phrase :=
     (fun l ->
       ignore(f());
       Toploop.parse_toplevel_phrase:=ocaml_init;
       ocaml_init l)
-
